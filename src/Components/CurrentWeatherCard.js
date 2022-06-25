497b8c76fd54a447f94aeb44fdcef0e96eef66ae
tauhid <tauhid495@gmail.com>
2022-06-25 13:52:13 +0600

    yea

--- src/Components/CurrentWeatherCard.js
+++ src/Components/CurrentWeatherCard.js
@@ -1,12 +1,20 @@
 import React, { } from 'react';
 
-const CurrentWeatherCard = ({ cityName, country }) => {
+const CurrentWeatherCard = ({ cityName, country, date, temp, img, text }) => {
 
 
     return (
         <div className='px-10 text-xl'>
-            <h1 className='text-3xl mt-4 mb-8'>{cityName}, {country}</h1>
-            <h3 className='text-xl'>Date</h3>
+            <h1 className='text-3xl mt-4 '>{cityName}, {country}</h1>
+            <h3 className='text-xl'>Date & time : {date}</h3>
+            <div className='flex justify-between'>
+                {/* degree sign= alt + 0176 */}
+                <div className='text-5xl my-4'>{temp} °C</div>
+                <div className='flex flex-col justify-center items-center'>
+                    <img src={img} alt="weather condition pic" />
+                    <p className='text-base'>{text}</p>
+                </div>
+            </div>
             <div className='flex justify-between my-3'>
                 <p>Tempareture</p>
                 <p>Humidity</p>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,11 @@ const Home = ({ findCity }) => {
     const [city, setCity] = useState(null);
     const [cityName, setCityName] = useState(null);
     const [country, setCountry] = useState(null);
+    const [date, setDate] = useState(null);
+    const [temp, setTemp] = useState(null);
+    const [img, setImg] = useState(null);
+    const [text, setText] = useState(null);
+
 
 
     useEffect(() => {
@@ -24,6 +29,11 @@ const Home = ({ findCity }) => {
                 console.log(weatherData.data);
                 setCityName(weatherData.data.location.name)
                 setCountry(weatherData.data.location.country)
+                setDate(weatherData.data.location.localtime)
+                setTemp(weatherData.data.current.temp_c)
+                setImg(weatherData.data.current.condition.icon)
+                setText(weatherData.data.current.condition.text)
+
             })
     }, [city]);
 
@@ -35,6 +45,10 @@ const Home = ({ findCity }) => {
                     <CurrentWeatherCard
                         cityName={cityName}
                         country={country}
+                        date={date}
+                        temp={temp}
+                        img={img}
+                        text={text}
                     />
                 </div>
                 <div class="divider lg:divider-horizontal"></div>
